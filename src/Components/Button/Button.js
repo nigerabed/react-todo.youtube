8499e3b46768d708d5bd888fcd159805111f98c6
Niger <nigerabed@gmail.com>
2024-05-02 11:34:44 +0200

    create button component with props

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,5 @@
+export default function Button(props){
+    return(
+        <button type="submit">{props.text}</button>
+    )
+}
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,11 @@
+import Button from "../Button/Button";
 import Input from "../Input/Input";
 
 export default function Form(){
     return(
         <form>
             <Input/>
+            <Button text="Add Task"/>
         </form>
     )
 }
